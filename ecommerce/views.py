ccaa78e174f7572c04fc54b4eaf45b0c751e31b8
shailesh <shailesh.kumarcs@gmail.com>
2018-07-10 23:06:06 +0530

    custom login and register has been comleted

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,13 +1,16 @@
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
     context = {
         'title': 'Hello World',
         'content': "Welcome to the home Page",
     }
+    if request.user.is_authenticated:
+        context['premium_content'] = "YAHUUU"
     return render(request, "home_page.html", context)
 
 def about_page(request):
@@ -18,18 +21,57 @@ def about_page(request):
     return render(request, "home_page.html", context)
 
 def contact_page(request):
-    contact_form = ContactForm()
+    contact_form = ContactForm(request.POST or None)
     context = {
         'title': 'Contact Page',
         'content': "Welcome to the contact Page",
         'form': contact_form
     }
-    if request.method == "POST":
-        #print(request.POST)
-        print(request.POST.get('fullname'))
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     #print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form,
+    }
+    print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            print(request.user.is_authenticated())
+            login(request, user)
+            # Redirect to a success page.
+            #context['form'] = LoginForm()
+            return redirect("/")
+        else:
+            # Return an 'invalid login' error message.
+            print("Error")
+    return render(request, "auth/login.html", context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form': form,
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, "auth/register.html", context)
 
 def home_page_old(request):
     html_ = """
