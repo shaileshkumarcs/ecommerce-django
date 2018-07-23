3445bc4d990f12a30b6aa1c275dd3b88e9be049d
shailesh <shailesh.kumarcs@gmail.com>
2018-07-23 22:13:29 +0530

    Image field create and upload

---
+++ products/views.py
@@ -0,0 +1,49 @@
+from django.http import Http404
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+
+def Product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list' : queryset
+    }
+    return render(request, "products/list.html",context)
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+
+def Product_detail_view(request, pk=None, *args, **kwargs):
+    #instance = Product.objects.get(pk=pk) #id
+    #instance = get_object_or_404(Product, pk=pk)
+    # try:
+    #     instance = Product.objects.get(id=pk)
+    # except Product.DoesNotExist:
+    #     print("No Product Here")
+    #     raise Http404("Product doesn't exist")
+    # except:
+    #     print('huh?')
+
+    qs = Product.objects.filter(id=pk)
+    if qs.exists() and qs.count() == 1:
+        instance = qs.first()
+    else:
+        raise Http404("Product doesn't exist")
+
+    context = {
+        'object' : instance
+    }
+    return render(request, "products/detail.html",context)
