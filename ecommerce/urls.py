496f8a6dc1204414f5dd4d7ad72f3448928de1e1
shailesh <shailesh.kumarcs@gmail.com>
2018-07-24 22:16:00 +0530

    Slugification completed

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,6 +23,7 @@ from products.views import (
                 ProductListView,
                 Product_list_view,
                 ProductDetailView,
+                ProductDetailSlugView,
                 Product_detail_view,
                 ProductFeaturedListView,
                 ProductFeaturedDetailView,
@@ -42,7 +43,8 @@ urlpatterns = [
     url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
     url(r'^products/$', ProductListView.as_view()),
     url(r'^products-fbv/$', Product_list_view),
-    url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    #url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    url(r'^products/(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
     url(r'^products-fbv/(?P<pk>\d+)/$', Product_detail_view),
     url(r'^admin/', admin.site.urls),
 
