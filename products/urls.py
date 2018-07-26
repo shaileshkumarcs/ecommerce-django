dbc31cc0f5788d595d707fae349a7b2c6e025d72
shailesh <shailesh.kumarcs@gmail.com>
2018-07-26 22:03:48 +0530

    Reverse URL

--- products/urls.py
+++ products/urls.py
@@ -14,9 +14,9 @@ from products.views import (
 urlpatterns = [
     # url(r'^featured/$', ProductFeaturedListView.as_view()),
     # url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
-    url(r'^$', ProductListView.as_view()),
+    url(r'^$', ProductListView.as_view(), name="list"),
     #url(r'^products-fbv/$', Product_list_view),
     #url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
-    url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
+    url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(),name="detail"),
     #url(r'^products-fbv/(?P<pk>\d+)/$', Product_detail_view),
 ]
